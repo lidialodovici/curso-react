1df4ac952e9057067a215e0d919c5f4cdc191017
Lídia Lodovici <lidia.lodovici@corebiz.ag>
2022-08-07 18:43:51 -0300

    Aula 0209: Eventos

--- src/App.js
+++ src/App.js
@@ -1,52 +1,23 @@
 import React from 'react';
 
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
-
 const App = () => {
+  function handleScroll(e) {
+    console.log(e);
+  }
+  window.addEventListener('scroll', handleScroll);
+
+  function handleClick(event) {
+    console.log(event);
+  }
   return (
-    <section>
-      {produtos
-        .filter(({ preco }) => Number(preco.replace('R$', '')) >= 2000)
-        .map(({ id, nome, preco, cores }) => (
-          <div key={id}>
-            <h1>{nome}</h1>
-            <p>{preco}</p>
-            <ul>
-              {cores.map((cor, index) => (
-                <li
-                  style={{
-                    backgroundColor: cor,
-                    color: '#fff',
-                    listStyle: 'none',
-                  }}
-                  key={index}
-                >
-                  {cor}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-    </section>
+    <div style={{ height: '200vh' }}>
+      <button onClick={handleClick}>Clique</button>
+      <br />
+      <br />
+      <button onClick={(event) => alert(event.target.innerText)}>
+        Compre Agora
+      </button>
+    </div>
   );
 };
 
